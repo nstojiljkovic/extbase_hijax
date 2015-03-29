0df1a4b9a2f989c3ada57e0dc26b7853d798c075
Nikola Stojiljkovic <nikola.stojiljkovic@essentialdots.com>
2015-03-29 14:33:45 +0200

    [TASK] Set simple fields during initial inserting of the persisted entity to the DB
    Change-Id: I9b98fd7e774c5ad747854f80640fd22c4b9ab1a7
    Related: #1812

--- Classes/Persistence/Backend.php
+++ Classes/Persistence/Backend.php
@@ -23,6 +23,7 @@ namespace EssentialDots\ExtbaseHijax\Persistence;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use TYPO3\CMS\Extbase\Persistence\ObjectMonitoringInterface;
 
 /**
  * Class Backend
@@ -60,13 +61,83 @@ class Backend extends \TYPO3\CMS\Extbase\Persistence\Generic\Backend {
 	/**
 	 * Inserts an object in the storage backend
 	 *
-	 * @param \TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $object The object to be insterted in the storage
+	 * @param \TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $object
+	 * @param \TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $parentObject
+	 * @param string $parentPropertyName
+	 * @throws \TYPO3\CMS\Extbase\Persistence\Generic\Exception
+	 * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotException
+	 * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException
 	 * @return void
 	 */
-	protected function insertObject(\TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $object) {
+	protected function insertObject(\TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $object, \TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface $parentObject = NULL, $parentPropertyName = '') {
 		$this->signalSlotDispatcher->dispatch('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Backend', 'beforeInsertObjectHijax', array('object' => $object));
 
-		parent::insertObject($object);
+		if (FALSE) {
+			parent::insertObject($object);
+		} else {
+			if ($object instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractValueObject) {
+				$result = $this->getUidOfAlreadyPersistedValueObject($object);
+				if ($result !== FALSE) {
+					$object->_setProperty('uid', (int)$result);
+					return;
+				}
+			}
+			$dataMap = $this->dataMapper->getDataMap(get_class($object));
+			$row = array();
+			$this->addCommonFieldsToRow($object, $row);
+			if ($dataMap->getLanguageIdColumnName() !== NULL) {
+				$row[$dataMap->getLanguageIdColumnName()] = -1;
+			}
+
+			$dataMap = $this->dataMapper->getDataMap(get_class($object));
+			$properties = $object->_getProperties();
+			foreach ($properties as $propertyName => $propertyValue) {
+				if (!$dataMap->isPersistableProperty($propertyName) || $this->propertyValueIsLazyLoaded($propertyValue)) {
+					continue;
+				}
+				$columnMap = $dataMap->getColumnMap($propertyName);
+				if ($propertyValue instanceof \TYPO3\CMS\Extbase\Persistence\ObjectStorage) {
+					// just skip, it's too much to deal with ObjectStorage atm
+					continue;
+				} elseif ($propertyValue instanceof \TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface
+					&& $object instanceof ObjectMonitoringInterface) {
+					if ($object->_isDirty($propertyName)) {
+						if (!$propertyValue->_isNew()) {
+							$row[$columnMap->getColumnName()] = $this->getPlainValue($propertyValue);
+							if ($object instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractEntity) {
+								$object->_memorizeCleanState($propertyName);
+							}
+						}
+					}
+				} else {
+					$row[$columnMap->getColumnName()] = $this->getPlainValue($propertyValue, $columnMap);
+					if ($object instanceof \TYPO3\CMS\Extbase\DomainObject\AbstractEntity) {
+						$object->_memorizeCleanState($propertyName);
+					}
+				}
+			}
+
+			if ($parentObject !== NULL && $parentPropertyName) {
+				$parentColumnDataMap = $this->dataMapper->getDataMap(get_class($parentObject))->getColumnMap($parentPropertyName);
+				$relationTableMatchFields = $parentColumnDataMap->getRelationTableMatchFields();
+				if (is_array($relationTableMatchFields)) {
+					$row = array_merge($relationTableMatchFields, $row);
+				}
+				if ($parentColumnDataMap->getParentKeyFieldName() !== NULL) {
+					$row[$parentColumnDataMap->getParentKeyFieldName()] = (int)$parentObject->getUid();
+				}
+			}
+			$uid = $this->storageBackend->addRow($dataMap->getTableName(), $row);
+			$object->_setProperty('uid', (int)$uid);
+			if ((int)$uid >= 1) {
+				$this->emitAfterInsertObjectSignal($object);
+			}
+			$frameworkConfiguration = $this->configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+			if ($frameworkConfiguration['persistence']['updateReferenceIndex'] === '1') {
+				$this->referenceIndex->updateRefIndexTable($dataMap->getTableName(), $uid);
+			}
+			$this->session->registerObject($object, $uid);
+		}
 
 		if ($object->getUid() >= 1) {
 			/*
