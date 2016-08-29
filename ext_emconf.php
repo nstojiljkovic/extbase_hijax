3e45b10cc00b2bbc7846e13c693aae172e1a0b31
Jenkins <jenkins@essentialdots.com>
2016-08-29 08:45:08 +0000

    [RELEASE] Release of extbase_hijax 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,7 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "extbase_hijax".
-#
-# Auto generated 28-03-2012 18:17
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Extbase Hijax add-on',
 	'description' => '',
 	'category' => 'plugin',
@@ -28,21 +19,27 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '3.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.1.5',
-			'extbase' => '',
-			'fluid' => '',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-			'ed_cache' => '0.2.5'
-		),
+	'version' => '3.0.0',
+	'constraints' => 
+	array (
+	  'depends' => 
+	  array (
+	    'typo3' => '6.1.5',
+	    'extbase' => '',
+	    'fluid' => '',
+	  ),
+	  'conflicts' => 
+	  array (
+	  ),
+	  'suggests' => 
+	  array (
+	    'ed_cache' => '0.2.5',
+	  ),
 	),
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
-	'conflicts' => ''
+	'conflicts' => '',
 );
 
+?>
