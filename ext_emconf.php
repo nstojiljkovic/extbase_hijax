ad58bd9cfa47a81ad12516d2148083a88eb1b7e1
Jenkins <jenkins@essentialdots.com>
2016-09-17 11:49:01 +0000

    [RELEASE] Release of extbase_hijax 3.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '3.0.4-dev',
+	'version' => '3.0.4',
 	'constraints' => 
 	array (
 	  'depends' => 
