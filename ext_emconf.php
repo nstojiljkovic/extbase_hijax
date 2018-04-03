a688ec66784617548e9a510b4be2cf2765d6988c
Jenkins <jenkins@essentialdots.com>
2018-04-03 13:26:48 +0000

    [TASK] Set extbase_hijax version to 3.0.9-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '3.0.8',
+	'version' => '3.0.9-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
