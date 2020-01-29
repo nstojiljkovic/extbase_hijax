9469fd338f2cb70456a7562346fd4777669e33ed
Jenkins <jenkins@essentialdots.com>
2020-01-29 10:16:44 +0000

    [RELEASE] Release of extbase_hijax 3.0.11

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '3.0.11-dev',
+	'version' => '3.0.11',
 	'constraints' => 
 	array (
 	  'depends' => 
