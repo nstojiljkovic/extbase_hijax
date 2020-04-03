7d87fc4b8438f63062ceb73ad399d08dd608adb7
Jenkins <jenkins@essentialdots.com>
2020-04-03 08:43:18 +0000

    [RELEASE] Release of extbase_hijax 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '4.0.1-dev',
+	'version' => '4.0.1',
 	'constraints' => 
 	array (
 	  'depends' => 
