821a4403211c05be171f503cfd64b8323fedde3f
Jenkins <no-reply@essentialdots.com>
2021-12-16 16:58:51 +0000

    [TASK] Set extbase_hijax version to 5.0.4-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '5.0.3',
+	'version' => '5.0.4-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
